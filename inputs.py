e9a80b5079526a6ad402e9fef5badd5d8a4576e7
xKiranjit <95500506+xKiranjit@users.noreply.github.com>
2021-12-20 16:04:01 +0800

    Delete inputs.py

--- inputs.py
+++
@@ -1,8 +0,0 @@
-'''names = input("Enter the names :")
-print(names)
-namelist = names.replace(" ","").split(",")
-print(namelist)
-'''
-a = int(input("enter first digit:"))
-b = int(input("enter 2nd digit:"))
-print(a*b)
